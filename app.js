71c7634b59262872c5a410ea69d278aabf2b8ca5
elvisb155 <elvisb155@hotmail.com>
2021-04-26 14:23:09 -0500

    added tone players

--- app.js
+++ app.js
@@ -23,21 +23,30 @@ const audio = document.querySelector('#audio');
 //   ).toDestination();
 let model; 
 
-let player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12;
-player1 = new Tone.Player('vc1.mp3').toDestination();
-player2 = new Tone.Player('vc2.mp3').toDestination();
-player3 = new Tone.Player('vc3.mp3').toDestination();
-player4 = new Tone.Player('vc4.mp3').toDestination();
-player5 = new Tone.Player('vc5.mp3').toDestination();
-player6 = new Tone.Player('vc6.mp3').toDestination();
-player7 = new Tone.Player('vc7.mp3').toDestination();
-player8 = new Tone.Player('vc8.mp3').toDestination();
-player9 = new Tone.Player('vc9.mp3').toDestination();
-player10 = new Tone.Player('vc10.mp3').toDestination();
-player11 = new Tone.Player('vc11.mp3').toDestination();
-player12 = new Tone.Player('vc12.mp3').toDestination();
+let player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21;
+player1 = new Tone.Player('/audios/vc1.mp3').toDestination();
+player2 = new Tone.Player('/audios/vc2.mp3').toDestination();
+player3 = new Tone.Player('/audios/vc3.mp3').toDestination();
+player4 = new Tone.Player('/audios/vc4.mp3').toDestination();
+player5 = new Tone.Player('/audios/vc5.mp3').toDestination();
+player6 = new Tone.Player('/audios/vc6.mp3').toDestination();
+player7 = new Tone.Player('/audios/vc7.mp3').toDestination();
+player8 = new Tone.Player('/audios/vc8.mp3').toDestination();
+player9 = new Tone.Player('/audios/vc9.mp3').toDestination();
+player10 = new Tone.Player('/audios/vc10.mp3').toDestination();
+player11 = new Tone.Player('/audios/vc11.mp3').toDestination();
+player12 = new Tone.Player('/audios/vc12.mp3').toDestination();
+player13 = new Tone.Player('/audios/vc13.mp3').toDestination();
+player14 = new Tone.Player('/audios/vc14.mp3').toDestination();
+player15 = new Tone.Player('/audios/vc15.mp3').toDestination();
+player16 = new Tone.Player('/audios/vc16.mp3').toDestination();
+player17 = new Tone.Player('/audios/vc17.mp3').toDestination();
+player18 = new Tone.Player('/audios/vc18.mp3').toDestination();
+player19 = new Tone.Player('/audios/vc19.mp3').toDestination();
+player20 = new Tone.Player('/audios/vc20.mp3').toDestination();
+player21 = new Tone.Player('/audios/vc21.mp3').toDestination();
 
-let allPlayers = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12];
+let allPlayers = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20, player21];
 
 Tone.loaded().then(() => {
 
@@ -98,11 +107,15 @@ model.detect(video) .then(predictions => {
   } else if (y > 200) {
     console.log(y, ", ", x)
        } else if (x < 100) {
+        if(player4.state === "stopped"){
          triggerPlayback(player4)
+        }
           // player4.start()
         } else if (x > 200) {
+          if(player5.state === "stopped"){
           triggerPlayback(player5);
         } else if (x > 300) {
+          if(player6.state === "stopped"){
           triggerPlayback(player6);
         } 
     }
