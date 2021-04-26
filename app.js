86873e8c3fd7b5000c5e71a7b7a8f016f4b4f806
elvisb155 <elvisb155@hotmail.com>
2021-04-26 11:28:48 -0500

    updated app html
    html= eliminated audio
    js= fixed audio triggering

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const modelParams = {
-    flipHorizontal: true,   // flip e.g for video 
-    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
-    maxNumBoxes: 2,        // maximum number of boxes to detect
-    iouThreshold: 1.1,      // ioU threshold for non-max suppression
-    scoreThreshold: 1.1    // confidence threshold for predictions.
-  }
-  
-  navigator.getUserMedia = 
-  navigator.getUserMedia || 
-  navigator.webkitGetUserMedia || 
-  navigator.mozGetUserMedia || 
-  navigator.msGetUserMedia;
+  flipHorizontal: true,   // flip e.g for video 
+  imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+  maxNumBoxes: 2,        // maximum number of boxes to detect
+  iouThreshold: 1.1,      // ioU threshold for non-max suppression
+  scoreThreshold: 1.1    // confidence threshold for predictions.
+}
 
-  //SELECT EVERYTHING FROM HTML
+navigator.getUserMedia = 
+navigator.getUserMedia || 
+navigator.webkitGetUserMedia || 
+navigator.mozGetUserMedia || 
+navigator.msGetUserMedia;
+
+//SELECT EVERYTHING FROM HTML
 
 const video = document.querySelector('#video');
 const audio = document.querySelector('#audio');
@@ -37,81 +37,106 @@ player10 = new Tone.Player('vc10.mp3').toDestination();
 player11 = new Tone.Player('vc11.mp3').toDestination();
 player12 = new Tone.Player('vc12.mp3').toDestination();
 
+let allPlayers = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12];
+
 Tone.loaded().then(() => {
-  
-  
+
+
 })
 
 document.querySelector('#stop').addEventListener('click', () => {  
-  // players.stopAll();
-  player1.stop();
-  player2.stop();
-  player3.stop();
-  player4.stop();
-  player5.stop();
-  player6.stop();
-  player7.stop();
-  player8.stop();
-  player9.stop();
-  player10.stop();
-  player11.stop();
-  player12.stop();
+stopAll();
+
+// players.stopAll();
+
+// allPlayers.forEach(player => {
+//   player.stop();
+// })
+
+// player1.stop();
+// player2.stop();
+// player3.stop();
+// player4.stop();
+// player5.stop();
+// player6.stop();
+// player7.stop();
+// player8.stop();
+// player9.stop();
+// player10.stop();
+// player11.stop();
+// player12.stop();
 
 })
 
+function stopAll() {
+allPlayers.forEach(player => {
+  player.stop();
+})
+}
+
 function runDetection(){
-  // console.log('detect')
-  model.detect(video) .then(predictions => {
-    if(predictions.length !==0){
-      let hand1 = predictions[0].bbox;
-      let x = hand1[0];
-      let y = hand1[1];
-      // let x = hand2[2];
-      // let y = hand2[3];
-      console.log(x)
-     
-      if (y > 100) {
-        if (x < 100) {
+// console.log('detect')
+model.detect(video) .then(predictions => {
+  if(predictions.length !==0){
+    let hand1 = predictions[0].bbox;
+    let x = hand1[0];
+    let y = hand1[1];
+    // let x = hand2[2];
+    // let y = hand2[3];
+    // console.log(x)
+   
+    if (y > 100) {
+      console.log(y, ", ", x)
+      if (x < 100) {
+        if(player1.state === "stopped"){
           player1.start()
-        } else if (x > 200) {
-          player2.start()
-        } else if (x > 300) {
+        }
+      } else if (x > 300) {
+        if(player3.state === "stopped"){
           player3.start()
-    } else if (y > 200) {
-         } else if (x < 100) {
-            player4.start()
-          } else if (x > 200) {
-            player5.start()
-          } else if (x > 300) {
-            player6.start()
-          } 
-      }
-      //PLAY THE SOUND
-      audio.play();
+        }
+  } else if (y > 200) {
+    console.log(y, ", ", x)
+       } else if (x < 100) {
+         triggerPlayback(player4)
+          // player4.start()
+        } else if (x > 200) {
+          triggerPlayback(player5);
+        } else if (x > 300) {
+          triggerPlayback(player6);
+        } 
     }
-  });
-      
+    //PLAY THE SOUND
+    // audio.play();
+  }
+});
+}
+
+function triggerPlayback(player) {
+if(player.state === "stopped"){
+  player.start()
+}
 }
 
 console.log(handTrack)
 
 handTrack.load().then(lmodel => {
-    console.log("model loaded")
-    model = lmodel;
-  })
-  
+  console.log("model loaded")
+  model = lmodel;
+})
+
 handTrack.startVideo(video).then(status => {
-  if(status){
-    console.log('Start Video')
-    navigator.getUserMedia({video: {}}, stream=> {
-        video.srcObject = stream;
-        //Run our detection
-            setInterval(runDetection, 100);
-            console.log('Start Detection');
-    },
-    err => console.log(err)
-    );
-  }
+if(status){
+  console.log('Start Video')
+  navigator.getUserMedia({video: {}}, stream=> {
+      video.srcObject = stream;
+      //Run our detection
+          setInterval(runDetection, 250);
+          console.log('Start Detection');
+  },
+  err => console.log(err)
+  );
+}
 });
 
 
@@ -120,8 +145,7 @@ handTrack.startVideo(video).then(status => {
 
 
 document.querySelector('#startAudio')?.addEventListener('click', async () => {
-	await Tone.start()
-	console.log('audio is ready')
- 
+await Tone.start()
+console.log('audio is ready')
+
 })
-  
